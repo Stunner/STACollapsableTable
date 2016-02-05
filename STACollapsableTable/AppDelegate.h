396c9ded35c3c15a53bc5db7446639bb6452ffae
Aaron Jubbal <ajjubbal@gmail.com>
2016-02-04 21:18:59 -0800

    Initial Commit

---
+++ STACollapsableTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STACollapsableTable
+//
+//  Created by Aaron Jubbal on 2/4/16.
+//  Copyright © 2016 Aaron Jubbal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
