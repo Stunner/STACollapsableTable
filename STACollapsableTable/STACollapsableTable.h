6d8d37221b24afa8a174a00292251d9990d77985
Aaron Jubbal <ajjubbal@gmail.com>
2016-02-13 02:46:04 -0800

    Adjustments to correct misplacement of STASearchOperation class files.

---
+++ STACollapsableTable/STACollapsableTable.h
@@ -0,0 +1,13 @@
+//
+//  STACollapsableTable.m
+//  STACollapsableTable
+//
+//  Created by Aaron Jubbal on 2/13/16.
+//  Copyright © 2016 Aaron Jubbal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "STACollapsableTableModel.h"
+#import "STATableModelSpecifier.h"
+#import "STACellModel.h"
